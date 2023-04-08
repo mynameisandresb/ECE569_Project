2c521f8286f658604cf8650e9c7a50eb70827329
Andres Barragan <andresbarragan@i16n20.ocelote.hpc.arizona.edu>
2023-04-07 20:01:37 -0700

    Added frame for black/white

--- cuda/BGS/main.cpp
+++ cuda/BGS/main.cpp
@@ -15,7 +15,7 @@
 // Use separable 1D filter or 2D Gaussian filter
 #define SEPARABLE_GAUSSIAN_FILTER 1
 // Do we do any preprocessing (blurring) at all?
-#define PREPROCESSING 0
+#define PREPROCESSING 1
 
 /**
 * function specifications for CUDA kernel calls
@@ -123,7 +123,7 @@ void test_cuda(){
   * create window to display results
   */
   cv::namedWindow("origin", CV_WINDOW_AUTOSIZE);
-
+  cv::namedWindow("result", CV_WINDOW_AUTOSIZE);
 
   /*
   * Absolute background
@@ -160,7 +160,7 @@ void test_cuda(){
   char buff[100];
 
   int i = 2;
-  std::string input_file = "../../Videos/streetLight/input/in000001.jpg";
+  std::string input_file = "../../Videos/logos/input/in000001.jpg";
 
   frame = readImage(input_file);
   if(!frame.isContinuous()){
@@ -334,18 +334,16 @@ void test_cuda(){
 
     t_parallel += t_parallel_f - t_parallel_s;
     cv::Mat temp = cv::Mat(numRows(), numCols(), CV_8UC1, binary);
-    #if PREPROCESSING == 1
-      cv::imshow("origin", dst);
-    #else
-      cv::imshow("origin", frame);
-    #endif
+    cv::imshow("origin", dst);
+    cvWaitKey(1);
+    cv::imshow("result", temp);
     cvWaitKey(1);
 
     //free up memory on the device
     cleanup();
 
     //get the next frame
-    sprintf(buff, "../../Videos/streetLight/input/in%06d.jpg", i++);
+    sprintf(buff, "../../Videos/logos/input/in%06d.jpg", i++);
     std::string buffAsStdStr = buff;
     const char * c = buffAsStdStr.c_str();
     frame = readImage(c);
@@ -356,16 +354,17 @@ void test_cuda(){
 
   //END LOOP and destroy the window
   cvDestroyWindow("origin");
+  cvDestroyWindow("result");
   t_total_f = cpu_timer();
   t_total = t_total_f-t_total_s;
   t_serial = t_total-t_parallel;
   
   // print timing information
-  // printf("%f %f %f %f %f\n", t_total, t_serial, t_parallel, t_gpu, t_parallel-t_gpu);
-  printf("Total Execution time: %f\n", t_total);
-  printf("Serial Execution part: %f\n", t_serial);
-  printf("Parallel Execution part: %f\n", t_parallel);
-  printf("Computation time for GPU: %f\n", t_gpu);
-  printf("Communication time for GPU: %f\n", t_parallel - t_gpu);
+  printf("%f %f %f %f %f\n", t_total, t_serial, t_parallel, t_gpu, t_parallel-t_gpu);
+  // printf("Total Execution time: %f\n", t_total);
+  // printf("Serial Execution part: %f\n", t_serial);
+  // printf("Parallel Execution part: %f\n", t_parallel);
+  // printf("Computation time for GPU: %f\n", t_gpu);
+  // printf("Communication time for GPU: %f\n", t_parallel - t_gpu);
 
 }
