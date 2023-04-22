72e1afd6cf5187b45457f8258339d25509fc83ed
Andres Barragan <andresbarragan@arizona.edu>
2023-04-22 00:01:12 -0700

    Correct images

--- cuda/BGS/main.cpp
+++ cuda/BGS/main.cpp
@@ -133,8 +133,8 @@ void test_cuda(){
   /*
   * create window to display results
   */
-  cv::namedWindow("origin", CV_WINDOW_AUTOSIZE);
-  cv::namedWindow("result", CV_WINDOW_AUTOSIZE);
+  // cv::namedWindow("origin", CV_WINDOW_AUTOSIZE);
+  // cv::namedWindow("result", CV_WINDOW_AUTOSIZE);
 
   /*
   * Absolute background
@@ -171,7 +171,7 @@ void test_cuda(){
   char buff[100];
 
   int i = 2;
-  std::string input_file = "../../Videos/logos/input/in000001.jpg";
+  std::string input_file = "../../video_converter/data/in000001.jpg";
 
   frame = readImage(input_file);
   if(!frame.isContinuous()){
@@ -278,18 +278,18 @@ void test_cuda(){
 
         preprocessGaussianBlur(&frame, &d_frame_to_blur, &d_frame_blurred, &d_blurred_temp, BLUR_SIZE);
         timer.Start();
-        // gaussian_and_median_blur(d_frame_to_blur,
-        //                 d_frame_blurred,
-        //                 d_blurred_temp,
-        //                 d_gaussian_filter,
-        //                 BLUR_SIZE,
-        //                 numRows(), numCols());
-        gaussian_and_median_shared_blur(d_frame_to_blur,
+        gaussian_and_median_blur(d_frame_to_blur,
                         d_frame_blurred,
                         d_blurred_temp,
                         d_gaussian_filter,
                         BLUR_SIZE,
                         numRows(), numCols());
+        // gaussian_and_median_shared_blur(d_frame_to_blur,
+        //                 d_frame_blurred,
+        //                 d_blurred_temp,
+        //                 d_gaussian_filter,
+        //                 BLUR_SIZE,
+        //                 numRows(), numCols());
         // gaussian_filter(d_frame_to_blur,
         //                 d_blurred_temp,
         //                 d_gaussian_filter,
@@ -359,16 +359,16 @@ void test_cuda(){
 
     t_parallel += t_parallel_f - t_parallel_s;
     cv::Mat temp = cv::Mat(numRows(), numCols(), CV_8UC1, binary);
-    cv::imshow("origin", dst);
-    cvWaitKey(1);
-    cv::imshow("result", temp);
-    cvWaitKey(1);
+    // cv::imshow("origin", dst);
+    // cvWaitKey(1);
+    // cv::imshow("result", temp);
+    // cvWaitKey(1);
 
     //free up memory on the device
     cleanup();
 
     //get the next frame
-    sprintf(buff, "../../Videos/logos/input/in%06d.jpg", i++);
+    sprintf(buff, "../../video_converter/data/in%06d.jpg", i++);
     std::string buffAsStdStr = buff;
     const char * c = buffAsStdStr.c_str();
     frame = readImage(c);
@@ -378,8 +378,8 @@ void test_cuda(){
   cudaFree(d_gaussian_filter);
 
   //END LOOP and destroy the window
-  cvDestroyWindow("origin");
-  cvDestroyWindow("result");
+  // cvDestroyWindow("origin");
+  // cvDestroyWindow("result");
   t_total_f = cpu_timer();
   t_total = t_total_f-t_total_s;
   t_serial = t_total-t_parallel;
