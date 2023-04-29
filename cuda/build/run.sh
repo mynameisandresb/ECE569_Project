6418c800ec0079cdf13f98793441a69852a16dae
Andres Barragan <andresbarragan@email.arizona.edu>
2023-04-29 01:18:49 -0700

    Added run script

---
+++ cuda/build/run.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+export PATH=/contrib/ece569/group1/.local/include:${PATH}
+export LD_LIBRARY_PATH=/contrib/ece569/group1/.local/lib
+export LIBRARY_PATH=/contrib/ece569/group1/.local/lib
+export CPLUS_INCLUDE_PATH=/contrib/ece569/group1/.local/include
+export PKG_CONFIG_PATH=/contrib/ece569/group1/.local/lib/pkgconfig
+
+bin/BGS
