8bfb5d0a44395db75bf2c4521c7934dbc752e9a7
JonGatesUA <103297467+JonGatesUA@users.noreply.github.com>
2023-04-29 15:17:16 -0700

    updated build run readme

--- HOG-Feature/run.sh
+++ HOG-Feature/run.sh
@@ -1,12 +1,5 @@
 #! /bin/bash
 
-if [ -z "$1" ]
-  then
-    FAST=0
-else
-    FAST=1
-fi
-
 export PATH=/contrib/ece569/group1/.local/include:${PATH}
 export LD_LIBRARY_PATH=/contrib/ece569/group1/.local/lib
 export LIBRARY_PATH=/contrib/ece569/group1/.local/lib
@@ -14,4 +7,12 @@ export CPLUS_INCLUDE_PATH=/contrib/ece569/group1/.local/include
 export PKG_CONFIG_PATH=/contrib/ece569/group1/.local/lib/pkgconfig
 export OpenCV_DIR=/contrib/ece569/group1/.local/share/OpenCV/
 
-./bin/BGS $FAST
+cd bin 
+
+if [ -z "$1" ]
+  then
+    ./HOG 0 0 0 0 0
+else
+    ./HOG 2 2 3 1 1
+fi
+
