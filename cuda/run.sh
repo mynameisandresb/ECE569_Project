c6155428dedecf993a31ec8d6bce4f7677b79b71
Andres Barragan <andresbarragan@arizona.edu>
2023-04-29 17:08:28 -0700

    Make same as John

--- cuda/run.sh
+++ cuda/run.sh
@@ -1,21 +1,5 @@
 #! /bin/bash
 
-# Disable and enable fast mode
-if [ -z "$1" ]
-  then
-    FAST=0
-else
-    FAST="$1"
-fi
-
-# Disable and enable display mode
-if [ -z "$2" ]
-  then
-    DISPLAY_MODE=0
-else
-    DISPLAY_MODE="$2"
-fi
-
 # Put open in path
 export PATH=/contrib/ece569/group1/.local/include:${PATH}
 export LD_LIBRARY_PATH=/contrib/ece569/group1/.local/lib
@@ -24,4 +8,11 @@ export CPLUS_INCLUDE_PATH=/contrib/ece569/group1/.local/include
 export PKG_CONFIG_PATH=/contrib/ece569/group1/.local/lib/pkgconfig
 export OpenCV_DIR=/contrib/ece569/group1/.local/share/OpenCV/
 
-./bin/BGS $FAST $DISPLAY_MODE
+
+# Disable and enable fast mode
+if [ -z "$1" ]
+  then
+    ./bin/BGS 0 1
+else
+    ./bin/BGS 1 1
+fi
